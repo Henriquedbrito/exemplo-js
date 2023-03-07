88ff2d9705ad1c0a51af7cb63d81db8cfdeef841
Henriquedbrito <rm98831@fiap.com.br>
2023-03-07 11:49:10 -0300

    script.js adicionado ao html.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Este é um comentario de linha
+
+/**
+ * É um comentario
+ * de 
+ * Bloco
+ */
+
+//Exemplo das funções console.
+console.log("Olá mundo!")
+console.error("Bom dia!")
+console.warn("Será?")
+console.info("Vai ser...")
